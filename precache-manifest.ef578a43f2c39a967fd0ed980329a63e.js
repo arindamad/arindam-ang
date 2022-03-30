39ef89da90f00ef49054f791b23517f1195b058b
arindamad <32026868+arindamad@users.noreply.github.com>
2022-03-30 20:59:08 +0530

    Update very frist music app

---
+++ precache-manifest.ef578a43f2c39a967fd0ed980329a63e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b05722cae257ac6c996230a9b850b7",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "bad044ad75bbf34e0ddf",
+    "url": "/social-network/static/css/main.037c107c.chunk.css"
+  },
+  {
+    "revision": "92e32b8aa674808102f7",
+    "url": "/social-network/static/js/2.abe1352d.chunk.js"
+  },
+  {
+    "revision": "bad044ad75bbf34e0ddf",
+    "url": "/social-network/static/js/main.95e5b682.chunk.js"
+  },
+  {
+    "revision": "cd8926f0a4b82e4e81df",
+    "url": "/social-network/static/js/runtime~main.eb74f080.js"
+  },
+  {
+    "revision": "8c6c3b7a01e4468bde1d7a3d8daf20fc",
+    "url": "/social-network/static/media/dj.8c6c3b7a.jpg"
+  },
+  {
+    "revision": "98b18ff459cfce3d69d9c9236e974e6b",
+    "url": "/social-network/static/media/music-note.98b18ff4.svg"
+  }
+]);
